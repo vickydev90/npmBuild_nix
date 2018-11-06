b2b0d224a504b9bfecef8312be6e8ad7cc944465
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2018-11-06 15:10:31 +0000

    removed perm line

--- src/com/jenkins/library/NodeJS.groovy
+++ src/com/jenkins/library/NodeJS.groovy
@@ -42,7 +42,6 @@ def getVersionFromPackageJSON() {
 
 def value() {
 	def config = libraryResource 'config.json'
-    	sh 'chmod 755 /tmp/config.json'
 	writeFile file: '/tmp/config.json', text: config
 	def inputFile = new File("/tmp/config.json")
 	def InputJSON = new JsonSlurper().parseText(inputFile.text)
