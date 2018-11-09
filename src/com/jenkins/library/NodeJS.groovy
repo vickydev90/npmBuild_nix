6951a73b8274416e030ed85e6da75fda66231a8a
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2018-11-09 14:11:14 +0000

    changed json call

--- src/com/jenkins/library/NodeJS.groovy
+++ src/com/jenkins/library/NodeJS.groovy
@@ -4,16 +4,14 @@ package com.jenkins.library
 import groovy.json.JsonSlurper
 
 def npm(runTarget) {
-   def jsonPrefix = json(configuration).prefix
+   def jsonPrefix = json(configuration)
+   println jsonPrefix.prefix
    sh """#!/bin/bash -e
-        ${jsonPrefix}
 	npm ${runTarget}"""
 }
 
 def npmRun(runTarget) {
-    def jsonPrefix = json().prefix
     sh """#!/bin/bash -e
-	${jsonPrefix}
         npm run ${runTarget}"""
 }
 
