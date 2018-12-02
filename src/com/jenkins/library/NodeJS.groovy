6bd5b31f693d2bb3c80f0b6d6a22fb2dcc523b10
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2018-12-02 20:51:16 +0000

    new packge with method in library

--- src/com/jenkins/library/NodeJS.groovy
+++ src/com/jenkins/library/NodeJS.groovy
@@ -27,7 +27,7 @@ def npmRun(runTarget, targetBranch, targetEnv, configuration) {
 	}
 	  writeFile file: '/tmp/package.sh', text: libraryResource('package.sh')
 	  def pack = "/tmp/package.sh"
-	  pack.exec(executable:"package.sh", osfamily:"unix")
+	  chmod(file:"/tmp/package.sh", perm:'+x', includes:"*")
 	  sh(returnStdout: true, script: pack)
 	  dir('j2') {
       stash name: "artifact-${context.application}-${targetBranch}", includes: artifact
