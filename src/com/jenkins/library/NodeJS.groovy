65f7d5574c99b5d79e74f86cc21049c903eb2f1d
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2018-12-13 17:14:48 +0000

    updated node function

--- src/com/jenkins/library/NodeJS.groovy
+++ src/com/jenkins/library/NodeJS.groovy
@@ -4,8 +4,9 @@ package com.jenkins.library
 
 import groovy.json.JsonSlurper
 
-def npm(runTarget, configuration) {
+def npm(runTarget) {
 	try{
+		runfunction()
 		sh """#!/bin/bash -e
 		 npm ${runTarget}"""
 	} catch (Exception ex) {
@@ -18,6 +19,7 @@ def npmRun(runTarget, targetBranch, targetEnv, configuration) {
 	String artifact = this.artifactName(targetBranch, targetEnv, configuration)
 	def context = json(configuration)
 	try{
+	 	runfunction()
 	    sh """#!/bin/bash -e
         npm run ${runTarget}"""
         
@@ -35,9 +37,12 @@ def npmRun(runTarget, targetBranch, targetEnv, configuration) {
       }
 }
 
-String copyGlobalLibraryScript() {
-  writeFile file: '/tmp/package.sh', text: libraryResource('package.sh')
-}
+def runfunction() {
+      writeFile file: '/tmp/functions.sh', text: libraryResource('functions')
+	  def func = "chmod +x /tmp/functions.sh"
+	  def sh = "./tmp/functions.sh"
+	  return sh
+    }
 
 
 def getVersionFromPackageJSON() {
@@ -53,16 +58,6 @@ def json(configuration) {
 	HashMap configFile  = (new HashMap(new groovy.json.JsonSlurperClassic().parseText(jfile))).asImmutable()
 	return configFile
 }
-
-//def EnvVar(configuration) {
-  //      def var = this.json(configuration)
-    //    for (item in var.npm_var) {
-      //       sh """ ${var.prefix}npm set """ ${item}   
-        //  }
-
-    //}
-
-
 
 
 String artifactName(String targetBranch, String targetEnv, configuration) {
