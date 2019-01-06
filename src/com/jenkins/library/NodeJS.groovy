08c2dff18446c91907211f5e7208cabb2bdfbb8b
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2019-01-06 14:31:13 +0000

    changed in pushnexus

--- src/com/jenkins/library/NodeJS.groovy
+++ src/com/jenkins/library/NodeJS.groovy
@@ -69,6 +69,8 @@ def publishNexus(targetEnv) {
   if (targetEnv == "integration-branch") {
   String artifact = this.artifactName(String env)
   //withCredentials([usernamePassword(credentialsId: 'nexusLocal', passwordVariable: 'pass', usernameVariable: 'test')]){
+  def nexusURL = context.nexus.url
+  def credentialsID = context.nexus.credentials
   def packageVersion = getVersionFromPackageJSON()
   def context = config()
   try {
@@ -76,12 +78,17 @@ def publishNexus(targetEnv) {
       deleteDir()
       unstash "artifact-${context.application}-${targetEnv}"
       artifact = sh(returnStdout: true, script: 'ls *.tar.gz | head -1').trim()
-      def nexusObject = new pushNexus()
-      nexusObject.call {
-          targetURL = {context.nexus.url}
-          tarfile = artifact
-          cred = {context.nexus.credentials}
-      }
+      withCredentials([
+       usernameColonPassword(    credentialsId: credentialsID,
+       variable: 'NEXUS_CREDS')
+   ]) { sh     """curl --insecure    -sS \
+                           -u $NEXUS_CREDS \
+                           --upload-file \
+                           ${artifact} \
+                           ${nexusURL}/${artifact} 
+               """  
+   }
+      
         }
   } catch (Exception ex) {
 		println "FAILED: export ${ex.message}"
