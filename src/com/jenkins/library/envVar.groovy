790ed4e992230acdda105725aaaa04da74a9fd92
Vishal Deshmukh <vishal.deshmukh@sapient.com>
2019-01-14 16:49:08 +0000

    printyln

--- src/com/jenkins/library/envVar.groovy
+++ src/com/jenkins/library/envVar.groovy
@@ -2,7 +2,7 @@ package com.jenkins.library
 
 public def VariablesName() {
  
-        // def ENV = [:]
-        //ENV.HTTP_PROXY='http://10.113.140.187:3128'
- 		println "Hello World"
+        def ENV = [:]
+        ENV.HTTP_PROXY='http://10.113.140.187:3128'
+ 		println $HTTP_PROXY
  }
